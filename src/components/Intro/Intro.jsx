ce50998279a2331df3489f2b424ffea5519fda90
rifai-rizqi3 <rifai.rizqi3@gmail.com>
2023-01-12 10:47:13 +0700

    pertama commit

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,96 @@
+import React, { useContext } from "react";
+import "./Intro.css";
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import boy from "../../img/boy.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
+import FloatinDiv from "../FloatingDiv/FloatingDiv";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+const Intro = () => {
+  // Transition
+  const transition = { duration: 2, type: "spring" };
+
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className="Intro" id="Intro">
+      {/* left name side */}
+      <div className="i-left">
+        <div className="i-name">
+          {/* yahan change hy darkmode ka */}
+          <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
+          <span>Rizqi Nur Rifai</span>
+          <span>
+          I am someone who is interested in the world of IT, Graphic Design, Graphic Video, Photography and the development of several web-based information applications. Therefore, I am very happy to implement and share what I already know and have with others, so that it becomes very useful, especially in the IT world which I have been working on for the past few years.
+          </span>
+        </div>
+        <Link to="contact" smooth={true} spy={true}>
+          <button className="button i-button">Hire me</button>
+        </Link>
+        {/* social icons */}
+        <div className="i-icons">
+        <a href="https://github.com/rifai-rizqi3" target="_blank" ><img src={Github} alt="Github" /></a>
+        <a href="https://www.linkedin.com/in/rnrifai/" target="_blank" ><img src={LinkedIn} alt="LinkedIn" /></a>
+        <a href="https://www.instagram.com/ii.hao/" target="_blank" ><img src={Instagram} alt="Instagram" /></a>
+        </div>
+      </div>
+      {/* right image side */}
+      <div className="i-right">
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={boy} alt="Rizqi" />
+        {/* animation */}
+        <motion.img
+          initial={{ left: "-36%" }}
+          whileInView={{ left: "-24%" }}
+          transition={transition}
+          src={glassesimoji}
+          alt=""
+        />
+
+        <motion.div
+          initial={{ top: "-4%", left: "75%" }}
+          whileInView={{ left: "65%" }}
+          transition={transition}
+          className="floating-div"
+        >
+          <FloatinDiv img={crown} text1="IT Support" />
+        </motion.div>
+
+        {/* animation */}
+        <motion.div
+          initial={{ left: "9rem", top: "18rem" }}
+          whileInView={{ left: "0rem" }}
+          transition={transition}
+          className="floating-div"
+        >
+          {/* floatinDiv mein change hy dark mode ka */}
+          <FloatinDiv img={thumbup} text1="Cyber" text2="Security" />
+        </motion.div>
+
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div
+          className="blur"
+          style={{
+            background: "#C1F5FF",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Toggle from "../Toggle/Toggle";
+import "./Navbar.css";
+import { Link } from "react-scroll";
+const navbar = () => {
+  return (
+    <div className="n-wrapper" id="Navbar">
+      {/* left */}
+      <div className="n-left">
+        <div className="n-name">RIZQI NUR RIFAI</div>
+        <Toggle />
+      </div>
+      {/* right */}
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: "none" }}>
+           
+          </ul>
+        </div>
+        <Link to="contact" spy={true} smooth={true}>
+        <button className="button n-button">Contact</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
